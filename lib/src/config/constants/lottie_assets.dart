be85527fc676a9f3ff1c3a2419ce24d3ea28fe5b
hamidjalili59 <hamidjalili9010@gmail.com>
2023-06-10 04:23:15 +0330

    fix: remove useless files

--- lib/src/config/constants/lottie_assets.dart
+++
@@ -1,9 +0,0 @@
-class LottieAssets{
-  const LottieAssets._();
-
-  static String get dotLoading => 'dot_loading'.lottie;
-}
-
-extension on String {
-  String get lottie => 'assets/lotties/$this.json';
-}
